9c10cc9604e0a03a660ceab4ef919efb2f3a50ad
Ali Bulut <ali@alibulut.net>
2021-06-13 15:04:58 +0300

    edit recipes tests added

---
+++ test/integration/recipes_edit_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class RecipesEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @chef = Chef.create!(chefname: "test chef", email: "test@test.com")
+    @recipe = Recipe.create(name: "test recipe", description: "test desc", chef: @chef)
+  end
+
+  test "reject invalid recipe update" do
+    get edit_recipe_path(@recipe)
+    assert_template 'recipes/edit'
+    patch recipe_path(@recipe), params: { recipe: { name: " ", description: "some desc" } }
+    assert_template 'recipes/edit'
+    assert_select "h2.panel-title"
+    assert_select "div.panel-body"
+  end
+
+  test "succesfully edit a recipe" do
+    get edit_recipe_path(@recipe)
+    assert_template 'recipes/edit'
+    updated_name = "updated recipe name"
+    updated_desc = "updated desc"
+    patch recipe_path(@recipe), params: { recipe: { name: updated_name, description: updated_desc } }
+    assert_redirected_to @recipe
+    # follow_redirect!
+    assert_not flash.empty?
+    @recipe.reload
+    assert_match updated_name, @recipe.name
+    assert_match updated_desc, @recipe.description
+  end
+end
