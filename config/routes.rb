085376502954cb3f99c7c24cdfd35da9c1ef5d39
Ali Bulut <ali@alibulut.net>
2021-06-13 14:42:57 +0300

    create recipes functionality completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "pages#home"
-  get "pages/home", to: "pages#home"
-  get "/recipes", to: "recipes#index"
-  get "/recipes/new", to: "recipes#new", as: "new_recipe"
-  get "/recipes/:id", to: "recipes#show", as: 'recipe'
+
+  resources :recipes
 end
