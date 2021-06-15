7d4e037201e8d71e1ea0cb7142ba9c5286833841
Ali Bulut <ali@alibulut.net>
2021-06-15 18:18:56 +0300

    edit chef feature implemented

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -7,7 +7,9 @@ class Chef < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 5 }
+  # we needed to use allow_nil for editing feature. It won't cause any problem while
+  # creating new chef because has_secure password always forces password to be not nil while creating
+  validates :password, presence: true, length: { minimum: 5 }, allow_nil: true
 
   has_many :recipes
   has_secure_password
